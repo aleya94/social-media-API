4b5a9b9e0a36674aa1c8a9d2691b3d6c698a38f5
Aleya Aly <kwaleya94@gmail.com>
2023-09-18 11:31:18 -0500

    social media api

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    public void register(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+            
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Account findbyusername(String username) {
+        Connection connection = ConnectionUtil.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT VALUES (?,?)")) {
+            preparedStatement.setString(1, username);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int account_id = resultSet.getInt("account_id");
+                    String password = resultSet.getString("password");
+                    return new Account(account_id, username, password);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Account findbyusernameAndpwd(String username, String password) {
+        return null;
+    } 
+}
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,28 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+public class AccountService {
+    private static AccountDAO accountDao;
+
+    public AccountService(){
+        accountDao = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDao){
+        AccountService.accountDao = accountDao;
+    }
+
+    public void registerAccount(Account account) {
+        accountDao.register(account);
+    }
+
+    public static Account findbyusername(String username) {
+        return accountDao.findbyusername(username);
+    }
+
+    public Account findbyusernameAndpwd(String username, String password) {
+        return accountDao.findbyusernameAndpwd(username, password);
+    }
+}
